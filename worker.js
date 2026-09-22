e37e3a9a1fa88dfca65a02b1063023b9022315cf
youantonio <132875678+youantonio@users.noreply.github.com>
2026-09-22 17:04:16 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -76,7 +76,7 @@ async function hashPassword(password, saltHex) {
     ["deriveBits"]
   );
   const bits = await crypto.subtle.deriveBits(
-    { name: "PBKDF2", salt: enc.encode(salt), iterations: 120000, hash: "SHA-256" },
+    { name: "PBKDF2", salt: enc.encode(salt), iterations: 100000, hash: "SHA-256" },
     key,
     256
   );
